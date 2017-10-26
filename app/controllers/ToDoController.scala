eaa14c1f7f6aea50e1e31efc852cf49eaccbd312
gentom <gento.m.developer@gmail.com>
2017-10-26 13:52:46 +0900

    commit

---
+++ app/controllers/ToDoController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+class ToDoController @Inject()(mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+
+  def hello() = Action {
+      implicit request: MessagesRequest[AnyContent] => Ok("Hi, I'm Play :D")
+  }
+
+}
