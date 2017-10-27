b3f72e978ead8c4ac31f6d2a9e80b72f7780be9b
gentom <gento.m.developer@gmail.com>
2017-10-27 15:27:17 +0900

    commit

--- app/controllers/ToDoController.scala
+++ app/controllers/ToDoController.scala
@@ -8,17 +8,23 @@ import play.api.data.Forms._
 // services
 import services._
 
-class ToDoController @Inject()(mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+class ToDoController @Inject()(todoService: ToDoService, mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
 
   def hello() = Action {
       implicit request: MessagesRequest[AnyContent] => Ok("Hi, I'm Play :D")
   }
 
-
+  /*
   def list() = Action {
     implicit request: MessagesRequest[AnyContent] =>
     val items: Seq[ToDo] = Seq(ToDo("ToDo1"), ToDo("ToDo2"))
     Ok(views.html.list(items))
-  } 
+  }
+  */
+
+  def list() = Action { implicit request: MessagesRequest[AnyContent] =>
+    val items: Seq[ToDo] = todoService.list()
+    Ok(views.html.list(items))
+  }
 
 }
--- app/services/ToDo.scala
+++ app/services/ToDo.scala
@@ -1,3 +1,34 @@
 package services
 
+import javax.inject.Inject 
+
+import anorm.SqlParser._
+import anorm._
+import play.api.db.DBApi
+
+import scala.language.postfixOps
+
+
+
 case class ToDo(name: String)
+
+@javax.inject.Singleton
+class ToDoService @Inject() (dbapi: DBApi){
+    private val db = dbapi.database("default")
+
+    val simple = {
+        get[String]("todo.name") map {
+            case name => ToDo(name)
+        }
+    }
+
+    def list(): Seq[ToDo] = {
+        db.withConnection { implicit connection =>
+            SQL(
+                """
+                select * from todo
+                """
+            ).as(simple *)
+        }
+    }
+}
