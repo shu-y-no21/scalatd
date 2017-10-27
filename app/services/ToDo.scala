b7e3bf18326a8512569dab186f9a61e6ff30858e
gentom <gento.m.developer@gmail.com>
2017-10-27 15:34:08 +0900

    commit

--- app/services/ToDo.scala
+++ app/services/ToDo.scala
@@ -1,6 +1,6 @@
 package services
 
-import javax.inject.Inject 
+import javax.inject.Inject
 
 import anorm.SqlParser._
 import anorm._
@@ -8,27 +8,31 @@ import play.api.db.DBApi
 
 import scala.language.postfixOps
 
-
-
 case class ToDo(name: String)
 
 @javax.inject.Singleton
-class ToDoService @Inject() (dbapi: DBApi){
-    private val db = dbapi.database("default")
+class ToDoService @Inject() (dbapi: DBApi) {
 
-    val simple = {
-        get[String]("todo.name") map {
-            case name => ToDo(name)
-        }
+  private val db = dbapi.database("default")
+
+  val simple = {
+    get[String]("todo.name") map {
+      case name => ToDo(name)
+    }
+  }
+
+  def list(): Seq[ToDo] = {
+
+    db.withConnection { implicit connection =>
+
+      SQL(
+        """
+          select * from todo
+        """
+      ).as(simple *)
+
     }
 
-    def list(): Seq[ToDo] = {
-        db.withConnection { implicit connection =>
-            SQL(
-                """
-                select * from todo
-                """
-            ).as(simple *)
-        }
-    }
+  }
+  
 }
